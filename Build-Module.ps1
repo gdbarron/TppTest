561794d225755f2c9f1d1a2393627cd9225d8b70
Brownstein <gbrownstein@deloitte.com>
2020-12-23 15:27:18 -0500

    testing

--- Build-Module.ps1
+++ Build-Module.ps1
@@ -245,12 +245,94 @@ if ($runIntegrationTests.IsPresent) {
 # Build the help
 if ($buildHelp.IsPresent) {
    Write-Output 'Processing: External help file'
-   Push-Location
-   Set-Location ./.docs
-   Try {
-      ./gen-help.ps1 -verbose
-   } Finally {
-      Pop-Location
+   # Push-Location
+   # Set-Location ./.docs
+   # Try {
+   #    ./gen-help.ps1 -verbose
+   # } Finally {
+   #    Pop-Location
+   # }
+   $branch = $env:BUILD_SOURCEBRANCHNAME
+
+   # $manifestPath = '..\Source\VenafiTppPS.psd1'
+   $manifest = Import-PowerShellDataFile .\Source\VenafiTppPS.psd1
+   [version]$version = $Manifest.ModuleVersion
+
+   "Loading Module from $manifestPath to update docs"
+   Remove-Module VenafiTppPS -Force -ea SilentlyContinue -Verbose
+   # platyPS + AppVeyor requires the module to be loaded in Global scope
+   Import-Module "$output/VenafiTppPS.psd1"
+
+   #Build YAMLText starting with the header
+   $YMLtext = (Get-Content ".\header-mkdocs.yml") -join "`n"
+   $YMLtext = "$YMLtext`n"
+   $parameters = @{
+      Path        = $releaseNotesPath
+      ErrorAction = 'SilentlyContinue'
    }
+   $ReleaseText = (Get-Content @parameters) -join "`n"
+   if ($ReleaseText) {
+      $ReleaseText | Set-Content ".\docs\RELEASE.md"
+      $YMLText = "$YMLtext  - Release Notes: RELEASE.md`n"
+   }
+   if ((Test-Path -Path $changeLogPath)) {
+      $YMLText = "$YMLtext  - Change Log: ChangeLog.md`n"
+   }
+   $YMLText = "$YMLtext  - Functions:`n"
+   # Drain the swamp
+   $parameters = @{
+      Recurse     = $true
+      Force       = $true
+      Path        = "$projectRoot\docs\functions"
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = Remove-Item @parameters
+   $Params = @{
+      Path        = ".\docs\functions"
+      type        = 'directory'
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = New-Item @Params
+   $Params = @{
+      Module       = $ModuleName
+      Force        = $true
+      OutputFolder = ".\docs\functions"
+      NoMetadata   = $true
+   }
+   New-MarkdownHelp @Params | ForEach-Object {
+      $Function = $_.Name -replace '\.md', ''
+      $Part = "    - {0}: functions/{1}" -f $Function, $_.Name
+      $YMLText = "{0}{1}`n" -f $YMLText, $Part
+      $Part
+   }
+   $YMLtext | Set-Content -Path ".\mkdocs.yml"
+
+   $YMLtext
+   Get-ChildItem ".\docs" -Recurse
+
+   try {
+      Write-Output ("Updating {0} branch source" -f $branch)
+      git.exe config user.email 'greg@jagtechnical.com'
+      git.exe config user.name 'Greg Brownstein'
+      # git.exe add *.psd1
+      git.exe add *.md
+      git.exe add ".\mkdocs.yml"
+      git.exe status -v
+      git.exe commit -m "Updated VenafiTppPS version to $version ***NO_CI***"
+
+      # if we are performing pull request validation, do not push the code to the repo
+      if ( $env:BUILD_REASON -eq 'PullRequest') {
+         Write-Output "Bypassing git push given this build is for pull request validation"
+      } else {
+         git.exe push https://$($GitHubPat)@github.com/gdbarron/TppTest.git ('HEAD:{0}' -f $branch)
+         # git.exe push https://$($GitHubPat)@github.com/gdbarron/VenafiTppPS.git ('HEAD:{0}' -f $branch)
+         Write-Output ("Updated {0} branch source" -f $branch)
+      }
+
+   } catch {
+      Write-Output ("Failed to update {0} branch with updated module metadata" -f $branch)
+      $_ | Format-List -Force
+   }
+
 }
 
